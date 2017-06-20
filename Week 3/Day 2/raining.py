2d8d249e669bb66312d2e9425c79e3286c4af717
M-Soma Institute <admissions.msoma@gmail.com>
2017-06-20 08:46:20 +0300

    Day 2 slides

---
+++ Week 3/Day 2/raining.py
@@ -0,0 +1,9 @@
+# Monicah Wambugu <--- Your name
+# fibonacci.py  <--- File Name
+# 19th June 2017  <--- Date
+
+raining = input('Is it raining today?: ')
+if raining == 'yes':
+	print('Carry your umbrella')
+else:
+	print('Wear your cap')
---
+++ Week 3/Day 2/test.py
@@ -0,0 +1,8 @@
+count = 0
+while (True):
+	count = count + 1
+	if (count >= 10):
+			break
+	print("tick tock")
+
+
